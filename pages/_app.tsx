7624ce17d4184a5590f85f4442caaf46a98f5ebf
Felix Schliemann <felix.schliemann@diva-e.com>
2020-10-06 16:48:12 +0200

    feat: renamed files to ts and added eslint

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+
+import { AppProps } from 'next/app';
+
+// eslint-disable-next-line react/jsx-props-no-spreading
+const MyApp = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+
+export default MyApp;
