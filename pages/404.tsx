c1951d89c560f0cf44017cb0089b8e81b4583631
Felix Schliemann <felix.schliemann@diva-e.com>
2020-11-28 16:49:14 +0100

    feat: add navigation to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,29 @@
+import { GetStaticProps } from 'next';
+import { StoryblokResult } from 'storyblok-js-client';
+
+import StoryblokService from '@utils/storyblok-service';
 import Layout from '@components/layout';
 
-const Custom404 = () => (
-  <Layout>
+const Custom404 = ({
+  settings,
+}: {
+  settings: StoryblokResult
+}) => (
+  <Layout settings={settings.data.story}>
     <h1>404 - Diese Seite konnte nicht gefunden werden</h1>
   </Layout>
 );
 
 export default Custom404;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const [settings] = await Promise.all([
+    StoryblokService.get('cdn/stories/settings', {}),
+  ]);
+
+  return {
+    props: {
+      settings,
+    },
+  };
+};
