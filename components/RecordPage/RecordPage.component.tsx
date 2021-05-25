44e3fefc95c9409d25164d8421a8b50c35f2e14c
Felix Schliemann <2472809+eisbr11@users.noreply.github.com>
2021-05-25 20:36:41 +0200

    feat: added Listening Links Menus to Record Detail pages

--- components/RecordPage/RecordPage.component.tsx
+++ components/RecordPage/RecordPage.component.tsx
@@ -5,6 +5,7 @@ import { MarkdownViewer } from '@components/Markdown';
 import CoverImage from './components/CoverImage';
 import Tracklist from './components/Tracklist';
 import ReleaseInfo from './components/ReleaseInfo';
+import ListenLinkDisplay from './components/ListenLinkDisplay';
 import useStyles from './RecordPage.styles';
 
 const RecordPage = ({
@@ -22,12 +23,21 @@ const RecordPage = ({
         <Grid item xs={12}>
           <Typography color="textPrimary" variant="h2" component="h1" className={classes.title}>{record.name}</Typography>
         </Grid>
-        <Grid className={classes.imageReleaseWrap} container item xs={12} md={6} lg={5}>
-          <Grid item xs={12} sm={6} md={12}>
-            <CoverImage coverImage={record.coverImage} />
-          </Grid>
-          <Grid item xs={12} sm={6} md={12}>
-            <ReleaseInfo releaseDate={record.releaseDate} formatType={record.formatType} />
+        <Grid item xs={12} md={6} lg={5}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6} md={12}>
+              <CoverImage coverImage={record.coverImage} />
+            </Grid>
+            <Grid item xs={12} sm={6} md={12}>
+              <ReleaseInfo releaseDate={record.releaseDate} formatType={record.formatType} />
+              <ListenLinkDisplay
+                bandcampLink={record.bandcampLink.cached_url}
+                spotifyLink={record.spotifyLink.cached_url}
+                tidalLink={record.tidalLink.cached_url}
+                deezerLink={record.deezerLink.cached_url}
+                youtubeMusicLink={record.youtubeMusicLink.cached_url}
+              />
+            </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12} md={6}>
