1ddd424be66c52b152c02280140d3fcd6fd6e818
Felix Schliemann <2472809+eisbr11@users.noreply.github.com>
2021-05-27 18:46:23 +0200

    feat: fetch fonts via google cdn to improve performance

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,6 +32,7 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;400;800&display=swap" rel="stylesheet" />
           <script
             dangerouslySetInnerHTML={{ __html: `var StoryblokCacheVersion = '${StoryblokService.getCacheVersion()}';` }}
           />
