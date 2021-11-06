9031f9b0bc53276a46844f735b424c417c6ec578
Felix Schliemann <2472809+eisbr11@users.noreply.github.com>
2021-11-06 15:32:31 +0100

    feat: pwa first draft version #2

--- next.config.js
+++ next.config.js
@@ -1,14 +1,11 @@
 const withPWA = require("next-pwa");
 
 module.exports = withPWA({
-  target: "serverless",
   images: {
     domains: ['a.storyblok.com'],
   },
   pwa: {
     disable: process.env.NODE_ENV === 'development',
-    dest: "public",
-    register: true,
-    skipWaiting: true,
+    dest: 'public',
   },
 });
