95997b04c77663926f622f2f25bd6c43462e2df6
Felix Schliemann <2472809+eisbr11@users.noreply.github.com>
2021-11-07 13:29:34 +0100

    feat: add option for external links (#32)

--- components/bloks/ContentLink.tsx
+++ components/bloks/ContentLink.tsx
@@ -1,14 +1,36 @@
+import React from 'react';
 import Link from 'next/link';
 import { Button, Typography } from '@mui/material';
 
 import { IBlokComponentProps } from '@interfaces/blok.interface';
 
-const ContentLink = ({ blok }: IBlokComponentProps) => (
-  <Typography paragraph align={blok.align}>
-    <Link href={blok.link.cached_url} passHref>
-      <Button aria-label={blok.text} variant={blok.variant} color={blok.color}>{blok.text}</Button>
-    </Link>
-  </Typography>
-);
+const ContentLink = ({ blok }: IBlokComponentProps) => {
+  return (
+    <Typography paragraph align={blok.align}>
+      {blok.target === '_blank' ? (
+        <Button
+          href={blok.link.cached_url}
+          target='_blank'
+          aria-label={blok.text}
+          variant={blok.variant}
+          color={blok.color}
+        >
+          {blok.text}
+        </Button>
+      ) : (
+        <Link href={blok.link.cached_url} passHref>
+          <Button
+            aria-label={blok.text}
+            variant={blok.variant}
+            color={blok.color}
+          >
+            {blok.text}
+          </Button>
+        </Link>
+      )}
+
+    </Typography>
+  );
+};
 
 export default ContentLink;
