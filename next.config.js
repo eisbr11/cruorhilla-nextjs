b6785f2463d6ad8dff8ab55ad144be0bc0c41c54
Felix Schliemann <fs@teamhero.de>
2023-03-24 10:53:13 +0100

    feat: update dependencies

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
-const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
+const withPWA = require("next-pwa")({
+  disable: process.env.NODE_ENV === 'development',
+  dest: 'public',
+  runtimeCaching,
+  skipWaiting: true,
+  buildExcludes: [/middleware-manifest\.json$/]
+});
 
 module.exports = withPWA({
   images: {
     domains: ['a.storyblok.com', 'img.youtube.com'],
-  },
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-    runtimeCaching,
-    skipWaiting: true,
-    buildExcludes: [/middleware-manifest\.json$/]
   },
 });
