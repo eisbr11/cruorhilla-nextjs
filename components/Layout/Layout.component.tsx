4ee957830c54832016f7d18fe05168155527330b
Felix Schliemann <fs@teamhero.de>
2025-07-05 12:18:46 +0200

    feat: remove unused Theme import in Layout component

--- components/Layout/Layout.component.tsx
+++ components/Layout/Layout.component.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { ThemeProvider, Theme, StyledEngineProvider } from '@mui/material/styles';
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { StoryData } from 'storyblok-js-client';
 
 import { useTheme } from '@context/theme.context';
 import getTheme from '@themes/index';
 import LayoutContent from './components/LayoutContent';
-
-declare module '@mui/styles/defaultTheme' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface DefaultTheme extends Theme {}
-}
 
 interface ILayoutComponentProps {
   children: React.ReactNode;
