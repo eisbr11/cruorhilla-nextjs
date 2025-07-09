455e25d1636476b6ca546c05d98137b0ded6f8e5
Felix Schliemann <fs@teamhero.de>
2025-07-09 11:31:23 +0200

    feat: simplify next.config.js by removing PWA configuration

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
-const runtimeCaching = require("next-pwa/cache");
-const withPWA = require("next-pwa")({
-  disable: process.env.NODE_ENV === 'development',
-  dest: 'public',
-  runtimeCaching,
-  skipWaiting: true,
-  buildExcludes: [/middleware-manifest\.json$/]
-});
-
-module.exports = withPWA({
+module.exports = {
   images: {
     remotePatterns: [
       new URL('https://a.storyblok.com/**'),
       new URL('https://img.youtube.com/**'),
     ],
   },
-});
+};
